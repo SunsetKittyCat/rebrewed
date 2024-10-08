2b51c88142a957193bacd4f5304fb7ad5434c796
SunsetCat <sunsetcat@lifelight.network>
2024-10-07 21:52:56 -0400

    Initial commit

---
+++ src/main/java/net/afternooncats/rebrewed/RebrewedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.afternooncats.rebrewed;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RebrewedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
